4491d0e5d75e85274090c555fc6f32a0a3dd527a
TranquillouMAN <beney7189@gmail.com>
2023-01-25 14:25:09 +0100

    lourd
    Co-authored-by: skybloc4 <skybloc4@users.noreply.github.com>
    Co-authored-by: AntoineXVI <AntoineXVI@users.noreply.github.com>
    Co-authored-by: Vosk <VoskbyVosk@users.noreply.github.com>
    Co-authored-by: KrstyBat <KrstyBat@users.noreply.github.com>

---
+++ action/update_article.php
@@ -0,0 +1,12 @@
+<?php 
+require_once "../cfg/config.php";
+$sql = "UPDATE user SET titre=:titre WHERE id=:id";
+$dataBinded=array(
+    ':id'   => $_POST['id'],
+    ':titre'=> $_POST['titre'],
+);
+$pre = $pdo->prepare($sql);
+$pre->execute($dataBinded);
+
+header('location:../admin.php')
+?>
